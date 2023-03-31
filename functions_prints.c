2e907b34fc48d14b7934b8c497244c9a2f171f09
andresrivera <andresrivera.web@gmail.com>
2023-03-30 21:46:42 -0700

    betty fix

--- functions_prints.c
+++ functions_prints.c
@@ -11,7 +11,6 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-
 /**
 * parser - Receives the main string and all the necessary parameters to
 * print a formated string.
@@ -27,7 +26,7 @@ printed_chars = 0;
 for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 {
 if (format[i] == '%') /*Checks for format specifiers*/
-{	  
+{
 /*Iterates through struct to find the right func*/
 for (j = 0; f_list[j].rm != NULL; j++)
 {
@@ -47,7 +46,7 @@ if (format[i + 1] != '\0')
 _putchar(format[i]);
 _putchar(format[i + 1]);
 printed_chars = printed_chars + 2;
-}	      
+}
 else
 return (-1);
 }
@@ -61,7 +60,6 @@ printed_chars++;
 }
 return (printed_chars);
 }
-
 /**
 * inttoascii - integer to ascii
 * @num: num
@@ -82,17 +80,17 @@ do {
 *--ptr = digits[n % base];
 n /= base;
 } while (n > 0);
-if (sign < 0) {
+if (sign < 0)
+{
 *--ptr = '-';
 }
-return ptr;
+return (ptr);
 }
-
 /**
 * print_BOH - print parameter ASCCI
 * @str: parameter
-*
-* Return 0 
+* Descrition: print
+* Return: Always 0
 */
 int print_BOH(char *str)
 {
